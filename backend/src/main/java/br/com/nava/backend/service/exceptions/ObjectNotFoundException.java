75876c99b4b7cf866ab001d2bac30fac409ebcec
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-13 00:14:14 -0300

    Add param String type

--- backend/src/main/java/br/com/nava/backend/service/exceptions/ObjectNotFoundException.java
+++ backend/src/main/java/br/com/nava/backend/service/exceptions/ObjectNotFoundException.java
@@ -2,7 +2,7 @@ package br.com.nava.backend.service.exceptions;
 
 public class ObjectNotFoundException extends RuntimeException {
 
-    public ObjectNotFoundException(Integer id) {
-        super("User not found. Id: " + id);
+    public ObjectNotFoundException(String type,Integer id) {
+        super(type + " not found. Id: " + id);
     }
 }
