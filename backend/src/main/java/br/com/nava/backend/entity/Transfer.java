34e6d93a3153daaa51054d2c817e9d166cfad161
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-14 21:23:26 -0300

    Transfer

---
+++ backend/src/main/java/br/com/nava/backend/entity/Transfer.java
@@ -0,0 +1,34 @@
+package br.com.nava.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@Setter
+@Table(name = "tb_transfer")
+public class Transfer implements Serializable {
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    private LocalDate today;
+    private LocalDate scheduledDate;
+    private Double amount;
+    private Double rate;
+    private Double totalAmount;
+    private String status;
+
+}
