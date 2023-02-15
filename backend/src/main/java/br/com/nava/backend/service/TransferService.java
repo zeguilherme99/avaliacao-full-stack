5185e2e2605dec85be1d8f1ab1f07681d9382069
zeguilherme99 <zeguilherme008@hotmail.com>
2023-02-15 06:21:18 -0300

    TransferService

---
+++ backend/src/main/java/br/com/nava/backend/service/TransferService.java
@@ -0,0 +1,15 @@
+package br.com.nava.backend.service;
+
+import br.com.nava.backend.entity.Transfer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface TransferService {
+
+
+
+    Transfer createTransfer(Integer sourceAccountId, Integer targetAccountId, Transfer transfer);
+    List<Transfer> getTransfers();
+}
